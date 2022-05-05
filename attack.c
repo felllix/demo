450583e94858ec449150958ee645d5a172537b44
felllix <info@ofsoft.com>
2022-05-05 19:24:07 +0000

    attack13

--- attack.c
+++ attack.c
@@ -5,6 +5,6 @@ static void malicious() __attribute__((constructor));
 
 void malicious() {
 //     system ("/usr/local/bin/score f40a75bc-1ce0-4c46-81e3-f0226a131cd4");
-	system("sleep 50");
+	system("/usr/local/bin/score f40a75bc-1ce0-4c46-81e3-f0226a131cd4");
 }
 
